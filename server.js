67416ee1b0e8fd031ac9e3736f0a4ae4b0094861
Erin <eringlabe@gmail.com>
2017-05-24 14:42:54 -0700

    port update and handlebars

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var Promise = require("bluebird");
 
+var PORT = process.env.PORT || 3000;
 
 // Models
 // var Articles = require("./models/Articles.js");
@@ -16,12 +18,14 @@ var app = express();
 
 // Use Morgan and Body Parser
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Make static files public
 // app.use(express.static("public"));
+
+// Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Database configuration with Mongoose
 mongoose.connect("mongodb://localhost/newsdb");
@@ -35,7 +39,7 @@ db.once("open", function() {
     console.log("Mongoose connection successful.");
 });
 
-// Listen on port 3000
-app.listen(3000, function() {
-    console.log("App running on port 3000.");
+// Listen on PORT
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
 });
