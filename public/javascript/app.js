b568ec36bc539a4d0377af378d39d98a4c98e1ec
Erin <eringlabe@gmail.com>
2017-05-27 09:35:56 -0700

    submit button logic

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,4 +1,40 @@
-$(document).on("click", "submit-btn", function() {
+$(document).ready(function() {
 
+    $("#submit-btn").on("click", function() {
+        var userInput = {
+            userName: $("#user-name").val().trim(),
+            userComment: $("#user-comment").val().trim()
+        };
+        console.log(userInput);
+
+        User.count({ 'username': userName }, function(err, count) {
+            if (count === 0) {
+                newUserComment(userInput);
+            } else {
+                establishedUserComment(userInput);
+            }
+        });
+
+    });
+
+    function newUserComment(userInput) {
+        $.ajax({
+            method: "POST",
+            url: "/new-user-comment",
+            data: userInput
+        }).done(function() {
+            window.location.href = "/home";
+        });
+    }
+
+    function establishedUserComment(userInput) {
+        $.ajax({
+            method: "POST",
+            url: "/established-user-comment",
+            data: userInput
+        }).done(function() {
+            window.location.href = "/home";
+        });
+    }
 
 });
